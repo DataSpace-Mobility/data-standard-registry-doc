bde2e29579c642e7dc49e5399f74124a29091e00
Abhishek Upperwal <a.upperwal@gmail.com>
2020-10-07 00:09:33 +0530

    Updates

---
+++ precache-manifest.6dee2ee10fc31f6c8bb1ba9be71f2a12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85eff1759bf132f2b6578e2fcb2ff8ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4ad4f49be362bede0d7",
+    "url": "/static/css/main.abab9b06.chunk.css"
+  },
+  {
+    "revision": "c29af479d301d982cae1",
+    "url": "/static/js/2.53351d7b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.53351d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ad4f49be362bede0d7",
+    "url": "/static/js/main.e5680ce3.chunk.js"
+  },
+  {
+    "revision": "e009585587502425c5d1",
+    "url": "/static/js/runtime-main.1cb56693.js"
+  },
+  {
+    "revision": "eb2716e963139a8d2dcff51da8da6439",
+    "url": "/static/media/logo.eb2716e9.svg"
+  }
+]);
