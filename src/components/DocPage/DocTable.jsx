9923f6b9b0ff53092e11de6c6eb29270376b152c
Abhishek Upperwal <a.upperwal@gmail.com>
2020-10-15 20:39:48 +0530

    stable stage

--- src/components/DocPage/DocTable.jsx
+++ src/components/DocPage/DocTable.jsx
@@ -4,15 +4,23 @@ import './DocTable.scss';
 
 function DocTable(props) {
 
-    let standardFileJSON = props.standardFileJSON
+    let standardFileJSONArr = props.standardFileJSONArr
 
-    function parseRows(i) {
+    function parseMessageRows(fields) {
         let res = []
-       standardFileJSON.messages[i].fields.forEach((f, idx) => {
+       fields.forEach((f, idx) => {
+           let typeLink = f.type
+           let repeated = ''
+           if(f.type[0] === f.type[0].toUpperCase()) {
+               typeLink = <a href={"#" + f.fullType}>{f.type}</a>
+           }
+           if(f.label === 'repeated') {
+               repeated = '[ ] '
+           }
            res.push(
                <tr key={idx}>
                     <td className="field-name">{f.name}</td>
-                    <td>{f.type}</td>
+                    <td>{repeated}{typeLink}</td>
                     <td>{f.description}</td>
                 </tr>
            )
@@ -20,47 +28,85 @@ function DocTable(props) {
        return res
     }
 
-    function renderTable(i) {
+    function renderMessageTable(obj) {
         return(
-            <div key={i} id={standardFileJSON.messages[i].name} className="msg-table">
+            <div key={obj.fullName} id={obj.longName} className="msg-table">
                 <div className="msg-name-box">
-                    {/* <span className="msg-name-tag">Message</span> */}
-                    <span className="msg-name">{standardFileJSON.messages[i].name}</span>
+                    <span className="msg-name">{obj.name}</span>
                 </div>
-                <p className="msg-description">{standardFileJSON.messages[i].description}</p>
+                <p className="msg-description">{obj.description}</p>
                 <table className="table">
                     <thead className="header">
                         <tr>
                         <th scope="col" width="10%">Field Name</th>
-                        <th scope="col" width="10%">Type</th>
+                        <th scope="col" width="20%">Type</th>
                         <th scope="col">Description</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {parseRows(i)}
+                        {parseMessageRows(obj.fields)}
                     </tbody>
                 </table>
             </div>
         )
     }
 
+    function renderEnumTable(obj) {
+        return(
+            <div key={obj.fullName} id={obj.longName} className="msg-table">
+                <div className="msg-name-box">
+                    <span className="msg-name">{obj.name}</span>
+                </div>
+                <p className="msg-description">{obj.description}</p>
+                <table className="table">
+                    <thead className="header">
+                        <tr>
+                        <th scope="col" width="10%">Number</th>
+                        <th scope="col" width="10%">Value</th>
+                        <th scope="col">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {parseEnumRows(obj.values)}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    function parseEnumRows(fields) {
+        let res = []
+       fields.forEach((f, idx) => {
+           res.push(
+               <tr key={idx}>
+                    <td className="field-name">{f.number}</td>
+                    <td>{f.name}</td>
+                    <td>{f.description}</td>
+                </tr>
+           )
+       })
+       return res
+    }
+
     function renderAllTables() {
         let res = []
-        for(let i=0; i<standardFileJSON.messages.length; i++) {
-            res.push(renderTable(i))
-        }
+        standardFileJSONArr.forEach(file => {
+            for(let i=0; i<file.messages.length; i++) {
+                res.push(renderMessageTable(file.messages[i]))
+            }
+        })
+
+        standardFileJSONArr.forEach(file => {
+            for(let i=0; i<file.enums.length; i++) {
+                res.push(renderEnumTable(file.enums[i]))
+            }
+        })
+
         return res
     }
 
     return(
         <section className="DocTable">
-            <div className="nav-path">Data Standards > Transit > GTFS</div>
-            <div className="standard-description">
-                <h1>General Transit Feed Specification</h1>
-                <h5>
-                    The General Transit Feed Specification (GTFS), also known as GTFS static or static transit to differentiate it from the GTFS realtime extension, defines a common format for public transportation schedules and associated geographic information. GTFS "feeds" let public transit agencies publish their transit data and developers write applications that consume that data in an interoperable way.
-                </h5>
-            </div>
             {renderAllTables()}
         </section>
     )
---
+++ src/components/DocPage/GraphView.jsx
@@ -0,0 +1,190 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3Hierarchy from 'd3-hierarchy';
+import * as d3Zoom from 'd3-zoom';
+import * as d3 from 'd3';
+
+import './GraphView.scss'
+
+function GraphView(props) {
+
+    let feedMsgFullName = ''
+    const treeContainer = useRef(null)
+
+    function processNode(objMap, nodeType, nodeName) {
+        let obj = objMap[nodeType]
+        if(nodeType.indexOf('.') === -1) {
+            return {
+                //name: nodeType + ': ' + (nodeName || '')
+                name: (nodeName || '')
+            }
+        }
+
+        let children = []
+        if('fields' in obj) {
+            
+            obj.fields.forEach(field => {
+                children.push(processNode(objMap, field.fullType, field.name))
+            })
+            
+        } else {
+            
+            /* obj.values.forEach(value => {
+                children.push({
+                    name: value.name
+                })
+            }) */
+        }
+
+        let nName = ''
+        if(nodeName != undefined) {
+            // nName = ': ' + nodeName
+            nName = nodeName
+        }
+
+        if(children.length == 0) {
+            return {
+                name: nName
+            }
+        }
+        
+        return {
+            //name: obj.name + nName,
+            name: nName,
+            children: children
+        }
+    }
+
+    function parseAsTree(standardFileJSONArr) {
+        console.log(treeContainer.current.getBoundingClientRect())
+        let objMap = {}
+        standardFileJSONArr.forEach(file => {
+            for(let i=0; i<file.messages.length; i++) {
+                objMap[file.messages[i].fullName] = file.messages[i]
+
+                if(file.messages[i].name == 'FeedMessage') {
+                    feedMsgFullName = file.messages[i].fullName
+                }
+            }
+        })
+
+        standardFileJSONArr.forEach(file => {
+            for(let i=0; i<file.enums.length; i++) {
+                objMap[file.enums[i].fullName] = file.enums[i]
+            }
+        })
+        
+        
+        let data = processNode(objMap, feedMsgFullName, undefined)
+
+        console.log(objMap)
+
+        var width = 1000
+        var height = 1000
+
+        console.log(d3.select('#tree-container').node().getBoundingClientRect())
+
+        const zoom = d3Zoom.zoom()
+                        .scaleExtent([0.5, 32])
+                        .on('zoom', (e) => {
+                            svgContainer.attr("transform", e.transform)
+                        })
+        
+
+        /* var svg = d3.select('#tree-container')
+            .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .call(zoom) */
+
+        var svg = d3.select('#tree-container-svg')
+            .call(zoom)
+        var svgG = svg.append('g')
+                .attr("transform", "translate(90, 150)")
+        var svgContainer = svgG.append('g')
+                
+
+        const root = d3Hierarchy.hierarchy(data)
+
+        root.dx = 13;
+        root.dy = width / (root.height + 2);
+
+        var cluster = d3Hierarchy.cluster().nodeSize([root.dx, root.dy])
+
+        cluster(root)
+
+        console.log(root)
+
+        svgContainer.append("g")
+            .attr("fill", "none")
+            .attr("stroke", "#bbb")
+            .attr("stroke-opacity", 0.4)
+            .attr("stroke-width", 1.5)
+        .selectAll("path")
+            .data(root.links())
+            .join("path")
+            .attr("d", d => `
+                M${d.target.y},${d.target.x}
+                C${d.source.y + root.dy / 2},${d.target.x}
+                ${d.source.y + root.dy / 2},${d.source.x}
+                ${d.source.y},${d.source.x}
+            `);
+
+        svgContainer.append("g")
+            .selectAll("circle")
+            .data(root.descendants())
+            .join("circle")
+            .attr("cx", d => d.y)
+            .attr("cy", d => d.x)
+            .attr("fill", d => d.children ? "#bbb" : "#eee")
+            .attr("r", 2.5)
+            .on('mouseover', (d, e) => {
+                console.log(d)
+                console.log(e)
+            })
+
+        svgContainer.append("g")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10)
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-width", 3)
+            .selectAll("text")
+            .data(root.descendants())
+            .join("text")
+            .attr("fill", d => d.children ? "white" : "white")
+            .attr("x", d => d.y)
+            .attr("y", d => d.x)
+            .attr("dy", d => d.children ? "1.51em" : "0.31em")
+            .attr("dx", d => d.children ? 8 : 6)
+            .text(d => d.data.name)
+            .filter(d => d.children)
+            .attr("text-anchor", "end")
+            .clone(true).lower()
+            .attr("stroke", "#222");
+
+            //svg.attr("viewBox", 0, 0, width, height)
+
+            
+
+
+    }
+
+    function drawTree() {
+        
+    }
+
+    useEffect(() => {
+        console.log('useEffect')
+        console.log(treeContainer)
+        parseAsTree(props.StandardJSON.files)
+    })
+
+    return(
+        <section className="GraphView">
+            <div id="tree-container" ref={treeContainer}>
+                <svg id="tree-container-svg"></svg>
+            </div>
+        </section>
+    )
+}
+
+export default GraphView;
