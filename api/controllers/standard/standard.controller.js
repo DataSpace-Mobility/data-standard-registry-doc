a246360cf63d4750596d8ccf2172c77bebd901d7
Harshit Grover <harshit.grover@signitysolutions.in>
2021-01-25 13:34:24 +0530

    Update standard.controller.js
    Updated a new method for simplicity using promises instead of async await

--- api/controllers/standard/standard.controller.js
+++ api/controllers/standard/standard.controller.js
@@ -3,34 +3,38 @@ var Standard = require('../../models/standard.model');
 var create = async (req, res, next) => {
     let data = req.body;
     const shortName = data.info.shortName;
-    const data_check = await Standard.findOne({info:{shortName:shortName}});
-    if(data_check){
-        res.json({
-            status:409,
-            message:"Duplicate entry found"
-        });
-    }
-    const api_auth_key = data.api_auth_key;
-    delete data['api_auth_key'];
-    if(process.env.api_auth_key === api_auth_key){
-        var newStandard = new Standard(data);
-        newStandard.save(function (err) {
-            if (err)
-                res.json({
-                    status:404,
-                    message:"Insert Unsucesessfull"
-                })
-            res.json({
-                status:200,
-                message: 'Insert Sucessfull'
-            });
-        });
-    }else{
-        res.json({
-            status:404,
-            message:"Invalid / No auth key found"
+    await Standard.find({"info.shortName":shortName})
+        .then(async result => {
+            if(result.length>0){
+                res.json({status:409,message:"Duplicate entry found"})
+            }else{
+                {
+                    const api_auth_key = data.api_auth_key;
+                    delete data['api_auth_key'];
+                    if(process.env.api_auth_key === api_auth_key){
+                        await Standard.create(data)
+                            .then(result => {
+                                res.json({
+                                    status:200,
+                                    message: 'Insert Sucessfull'
+                                });
+                            })
+                            .catch(err => {
+                                res.json({
+                                    status:404,
+                                    message:"Insert Unsucesessfull",
+                                    description:err
+                                })
+                            });
+                    }else{
+                        res.json({
+                            status:404,
+                            message:"Invalid / No auth key found"
+                        })
+                    }}
+            }
         })
-    }
+        .catch(err=> console.log(err));
 };
 
 var get = (req, res, next) => {
