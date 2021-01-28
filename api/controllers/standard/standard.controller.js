7100c03b4653fb7e664b0848e13d934b4f089b4c
Harshit Grover <harshit.grover@signitysolutions.in>
2021-01-28 16:58:13 +0530

    Added update method and formatted using prettier

--- api/controllers/standard/standard.controller.js
+++ api/controllers/standard/standard.controller.js
@@ -1,67 +1,92 @@
-var Standard = require('../../models/standard.model');
+var Standard = require("../../models/standard.model");
 
 var create = async (req, res, next) => {
-    let data = req.body;
-    const shortName = data.info.shortName;
-    await Standard.find({"info.shortName":shortName})
-        .then(async result => {
-            if(result.length>0){
-                res.json({status:409,message:"Duplicate entry found"})
-            }else{
-                {
-                    const api_auth_key = data.api_auth_key;
-                    if(data.api_auth_key && process.env.api_auth_key === api_auth_key){
-                        delete data['api_auth_key'];
-                        await Standard.create(data)
-                            .then(result => {
-                                res.json({
-                                    status:200,
-                                    message: 'Insert Successful'
-                                });
-                            })
-                            .catch(err => {
-                                res.json({
-                                    status:404,
-                                    message:"Standard creation failed",
-                                    description:err
-                                })
-                            });
-                    }else{
-                        res.json({
-                            status:404,
-                            message:"Invalid / No auth key found"
-                        })
-                    }}
-            }
-        })
-        .catch(err=> console.log(err));
+  let data = req.body;
+  const shortName = data.info.shortName;
+  await Standard.find({ "info.shortName": shortName })
+    .then(async (result) => {
+      if (result.length > 0) {
+        res.json({ status: 409, message: "Duplicate entry found" });
+      } else {
+        {
+          const api_auth_key = data.api_auth_key;
+          if (data.api_auth_key && process.env.api_auth_key === api_auth_key) {
+            delete data["api_auth_key"];
+            await Standard.create(data)
+              .then((result) => {
+                res.json({
+                  status: 200,
+                  message: "Insert Successful",
+                });
+              })
+              .catch((err) => {
+                res.json({
+                  status: 404,
+                  message: "Standard creation failed",
+                  description: err,
+                });
+              });
+          } else {
+            res.json({
+              status: 404,
+              message: "Invalid / No auth key found",
+            });
+          }
+        }
+      }
+    })
+    .catch((err) => console.log(err));
 };
 
 var get = (req, res, next) => {
-    Standard.find({}, function (err, standards) {
-        if (err)
-            return next(err);
-        res.json(standards)
-    });
+  Standard.find({}, function (err, standards) {
+    if (err) return next(err);
+    res.json(standards);
+  });
 };
 
-const getLatestByShortName = async (req,res) => {
+const getLatestByShortName = async (req, res) => {
+  const shortName = req.body.shortName;
+  await Standard.find({ "info.shortName": shortName })
+    .then((result) => {
+      if (result.length > 0) {
+        result = result[0];
+        result["versions"] = [result["versions"][result["versions"].length - 1]];
+        res.json(result).status(200);
+      }else{
+          res.json({status:404,message:`No data found for ${shortName}`})
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+const updateStandard = async (req, res) => {
     const shortName = req.body.shortName;
-    await Standard.find({
-        "$where":() => {return {"info.shortName":shortName}},
-        "$sort":() => {return {"versions.created_at":-1}},
-        "$slice":() => {return 1}
-    }).then(result => {
-        res.status(200).json(result[0])
-    }).catch(err => console.log(err))
-}
+    // Assuming input versions is an array
+    const versions = req.body.versions[0];
+    // what to update, options -> major, minor, or patch
+    const increment_standard = req.body.increment_standard;
+    await Standard.find({ "info.shortName": shortName })
+      .then(async (result) => {
+        if (result.length > 0) {
+          result = result[0];
+          result["version"] = [result["version"][result["version"].length - 1]];
+          version = result['versions'].version
+          version[increment_standard]++;
+          versions['version'] = version
+          await Standard.update({$push:{"versions":versions}})
+        }else{
+            res.json({status:404,message:`No data found for ${shortName}`})
+        }
+      })
+      .catch((err) => console.log(err));
+};
 
 var getById = (req, res, next) => {
-    Standard.find({}, function (err, standards) {
-        if (err)
-            return next(err);
-        res.json(standards)
-    });
+  Standard.find({}, function (err, standards) {
+    if (err) return next(err);
+    res.json(standards);
+  });
 };
 
-module.exports = { create, get, getById, getLatestByShortName }
+module.exports = { create, get, getById, getLatestByShortName, updateStandard };
