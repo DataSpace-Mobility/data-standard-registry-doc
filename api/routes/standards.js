620d045e94e228ef98e0c2e8c6928e021d0ecdb3
Harshit Grover <groverharshit76@gmail.com>
2021-01-31 15:50:03 +0530

    Updated the routes as well

--- api/routes/standards.js
+++ api/routes/standards.js
@@ -6,10 +6,10 @@ var StandardController = require('../controllers/standard/standard.controller');
 router.post('/', StandardController.create);
 router.get('/', StandardController.get);
 router.get('/:id', StandardController.getById);
-router.post('/getLatestbyShortName',StandardController.getLatestByShortName);
+router.post('/getLatestVersionByShortName',StandardController.getLatestVersionByShortName);
 router.post('/updateStandard',StandardController.updateStandard);
-router.post('/getAllByShortName',StandardController.getAllByShortName);
-router.post('/getAllStandardByUUID',StandardController.getAllStandardByUUID);
-router.post('/getLatestStandardByUUID',StandardController.getLatestStandardByUUID);
+router.post('/getAllVersionsByShortName',StandardController.getAllVersionsByShortName);
+router.post('/getAllVersionsByUUID',StandardController.getAllVersionsByUUID);
+router.post('/getLatestVersionByUUID',StandardController.getLatestVersionByUUID);
 
 module.exports = router;
